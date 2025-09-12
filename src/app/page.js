5b202290e7e6c2e375aa474a535a6cd45ef9aa3a
Mpho Vincent Makgwe <118048918+Mpho-vincent-makgwe@users.noreply.github.com>
2025-09-12 21:45:08 +0200

    space

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,6 @@
-// components/Dashboard.js
+//Dashboard.js
 'use client';
-
 import JobSeekerDashboard from '@/components/JobSeekerComponents/JobSeekerDashboard';
 export default function Dashboard() {
-
   return <JobSeekerDashboard/>
 }
