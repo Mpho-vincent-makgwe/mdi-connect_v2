6660a4689305bc03dd31124049206a7d4c49e096
Mpho Vincent Makgwe <118048918+Mpho-vincent-makgwe@users.noreply.github.com>
2025-09-13 23:16:12 +0200

    removed bug from our layout

--- src/app/LayoutWrapper.js
+++ src/app/LayoutWrapper.js
@@ -89,8 +89,7 @@ function ProtectedLayout({ children }) {
 
   return (
     <div className="flex h-screen overflow-hidden bg-gray-50">
-      {!hideLayout && user?.role !== 'admin' && <Sidebar />}
-      {!hideLayout && user?.role === 'admin' && <AdminSidebar />} {/* You'll need to create an AdminSidebar component */}
+      {!hideLayout && <Sidebar />}
       <div className={`flex-1 flex flex-col overflow-hidden ${!hideLayout && user?.role !== 'admin' ? 'lg:ml-64' : ''}`}>
         {!hideLayout && <Navbar />}
         <main className={`flex-1 overflow-y-auto ${!hideLayout ? 'p-6 mt-16' : 'p-0'}`}>
